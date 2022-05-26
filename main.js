6db37590710cd7afbc9f14226f0acb826d198307
amvit <73838043+amvit@users.noreply.github.com>
2022-05-26 19:54:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -14,9 +20,25 @@ function modelLoaded(){
 }
 function draw(){
     background('#e5fa05');
+    
+    document.getElementById("font_size").innerHTML = "Width and Height of text will be = " + difference +"px";
+    textSize(difference);
+    fill('#F90093');
+    text('Samvit', 50, 400)
+    font(noseX, noseY, difference);
 }
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
 }
+    
